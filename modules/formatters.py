bd19139f065d95e536a508f70135dc4171ad1f4b
Guillaume BERNARD-REYMOND <bernardr@cbgp-d-student1>
2026-09-08 12:02:21 +0200

    ruff format

--- modules/formatters.py
+++ modules/formatters.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+
 def rename_columns(df: pd.DataFrame) -> pd.DataFrame:
     mapping = {
         "weathercode (wmo code)": "code",
@@ -7,22 +8,36 @@ def rename_columns(df: pd.DataFrame) -> pd.DataFrame:
         "temperature_2m_min (°C)": "tmin",
         "precipitation_sum (mm)": "pluie",
         "windspeed_10m_max (km/h)": "vent",
-        "winddirection_10m_dominant (°)" : "direction"
+        "winddirection_10m_dominant (°)": "direction",
     }
     return df.rename(columns=mapping)
 
+
 def add_units(df: pd.DataFrame) -> pd.DataFrame:
-    units = {"tmax":"°C","tmin":"°C","pluie":"mm","vent":""}
-    for col,unit in units.items():
-        df[col] = df[col].astype(str)+f" {unit}"
+    units = {"tmax": "°C", "tmin": "°C", "pluie": "mm", "vent": ""}
+    for col, unit in units.items():
+        df[col] = df[col].astype(str) + f" {unit}"
     return df
 
+
 def format_dates(df: pd.DataFrame) -> pd.DataFrame:
-    jours=["lun","mar","mer","jeu","ven","sam","dim"]
-    mois=["janv","fév","mars","avr","mai","juin","juil","août",
-          "sept","oct","nov","déc"]
+    jours = ["lun", "mar", "mer", "jeu", "ven", "sam", "dim"]
+    mois = [
+        "janv",
+        "fév",
+        "mars",
+        "avr",
+        "mai",
+        "juin",
+        "juil",
+        "août",
+        "sept",
+        "oct",
+        "nov",
+        "déc",
+    ]
 
     for i in range(df.shape[1]):
-        d = pd.to_datetime(df.iloc[0,i])
-        df.iloc[0,i] = f"{jours[d.weekday()]}\n{d.day}\n{mois[d.month-1]}"
+        d = pd.to_datetime(df.iloc[0, i])
+        df.iloc[0, i] = f"{jours[d.weekday()]}\n{d.day}\n{mois[d.month - 1]}"
     return df
--- modules/icons.py
+++ modules/icons.py
@@ -1,17 +1,17 @@
-
 import json
 import urllib.request
 from PIL import Image
 
 
-with open("./data/ic.json","r") as f:
-    df_ic=json.load(f)
+with open("./data/ic.json", "r") as f:
+    df_ic = json.load(f)
 
-#fonction pour insérer les images
-def image_code(code, ax): 
+
+# fonction pour insérer les images
+def image_code(code, ax):
     code_int = int(float(code))
-    url = df_ic[str(code_int)]['day']['image'] 
-    image = Image.open(urllib.request.urlopen(url)) 
-    ax.imshow(image) 
-    ax.axis('off') 
+    url = df_ic[str(code_int)]["day"]["image"]
+    image = Image.open(urllib.request.urlopen(url))
+    ax.imshow(image)
+    ax.axis("off")
     return ax
